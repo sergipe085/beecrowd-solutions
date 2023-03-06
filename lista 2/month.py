1bd9ad0c04f28e71c7c442919890b8cef8ae792c
Sérgio <71034184+serjolas1@users.noreply.github.com>
2023-03-05 23:24:48 -0300

    month

---
+++ lista 2/month.py
@@ -0,0 +1,18 @@
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+num = int(input())
+
+print(months[num - 1])
